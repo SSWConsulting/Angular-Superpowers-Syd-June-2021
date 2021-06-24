cb8699a13873774c6f4d634987927ba46cd692be
JeanThirion <jean.thirion@esial.net>
2021-06-24 11:04:20 +1000

    Introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'fbc-root',
   templateUrl: './app.component.html',
+  // template: '<div> hello World </div>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebootcamp-crm-syd2021';
+  title = 'SSW Firebootcamp';
+
+  myDate = new Date();
+
+  // textChanged(event: any){
+  //   var newValue = event.target.value;
+
+  //   this.title = newValue;
+  // }
 }
